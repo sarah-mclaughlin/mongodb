186c28939feddbdd8c23a0fd6f9e8a0ee4d2edba
sarah-mclaughlin <sarahmclaughlin0gmail.com>
2018-05-01 18:14:52 +1200

    mongo

--- db.js
+++ db.js
@@ -1,41 +1,130 @@
 const MongoClient = require('mongodb').MongoClient;
-// const assert = require('assert');
+const assert = require('assert');
 
 // Connection URL
 const url = 'mongodb://localhost:27017';
 
-// Database Name
-// const dbName = 'myproject';
-
 // Use connect method to connect to the server
 MongoClient.connect(url, (err, client) => {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
+  assert.equal(null, err)
 
-  const db = client.db(dbName);
+  console.log("Connected successfully to server")
+
+  const db = client.db('myproject')
+
+  // addMum(db)
+  // addSiblings(db)
+  // updateMum(db)
+  // updatePeter(db)
+  // removePeter(db)
+  // findFamily(db)
+  // findKate(db)
+  // findParents(db)
+  // findCharacteristics(db)
+
   
-  client.close();
-});
 
-
-MongoClient.connect(url, (err, db) => {
-    db.collection('Employee').insertOne({
-        Employeeid: 4,
-        EmployeeName: "NewEmployee"
-    })
+  client.close()
 })
 
-MongoClient.connect(url, (err, db) => {
-  db.collection('Employee').updateOne({
-      "EmployeeName": "NewEmployee"
-  }, { $set: {"EmployeeName": "Mohan"}
+
+const addMum = (db) => {
+  db.collection('mycollection').insert(
+    {
+      name: "Robyn",
+      // surname: "McLaughlin",
+      relationship: "mother",
+      charactertistics: ["wise", "kind", "selfless"],
+      age: 56
     }
   )
-})
+}
 
-MongoClient.connect(url, (err, db) => {
-  db.collection('Employee').deleteOne(
-    {"EmployeeName": "Mohan"}
+const addSiblings = (db) => {
+  db.collection('mycollection').insertMany([
+    {
+      name: "Kate",
+      surname: "McLaughlin",
+      relationship: "sister",
+      charactertistics: ["strong", "stubborn", "annoying"],
+      age: 26
+    },
+    {
+      name: "Tom",
+      surname: "McLaughlin",
+      relationship: "brother",
+      charactertistics: ["funny", "annoying", "too cool"],
+      age: 24
+    }
+  ])
+}
+
+const updateMum = (db) => {
+  db.collection('mycollection').update(
+    {
+      name: "Robyn",
+    },
+    {
+      $set: {surname: "McLaughlin"}
+    }
   )
-})
+}
+
+const updatePeter = (db) => {
+  db.collection('mycollection').update(
+    {
+      name: "Peter",
+    },
+    {
+      name: "Peter",
+      surname: "McLaughlin",
+      relationship: "father",
+      charactertistics: ["generous", "smart", "wise"],
+      age: 58
+    },
+    {
+      upsert: true
+    }
+  )
+}
+
+const removePeter = (db) => {
+  db.collection('mycollection').remove(
+    {
+      name: "Peter"
+    }
+  )
+}
+
+// const findFamily = (db) => {
+//   db.collection('mycollection').find({})
+// }
+
+const findKate = (db) => {
+  db.collection('mycollection').find(
+    {
+      name: "Kate"
+    }
+  )
+}
+
+const findParents = (db) => {
+  db.collection('mycollection').find(
+    {
+      age: {$gt:40}
+    }
+  )
+}
+
+const findCharacteristics = (db) => {
+  db.collection('mycollection').find(
+    {
+      characteristics: "wise"
+    }
+  )
+}
+
+// can iterate over docs with a forEach
+
+
 
